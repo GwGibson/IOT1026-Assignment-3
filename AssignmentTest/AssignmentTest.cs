3fce6395b9fb0117189a9fe491cd568446a70455
Dabebe100 <130933203+Dabebe100@users.noreply.github.com>
2023-06-04 12:18:46 +0100

    Update AssignmentTest.cs
    AssignmnetTest Update

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -1,13 +1,17 @@
-﻿using Assignment;
+using Assignment;
 
 namespace AssignmentTest
 {
     [TestClass]
     public class AssignmentTests
     {
+        //<summary>
+        //Tests the Constructor of the Pack Class
+        //</summary>
         [TestMethod]
         public void ConstructorTest()
         {
+            //To fix:move class to public
             const int PackMaxItems = 10;
             const float PackMaxVolume = 20;
             const float PackMaxWeight = 30;
@@ -15,7 +19,9 @@ namespace AssignmentTest
 
             Assert.AreEqual(pack.GetMaxCount(), PackMaxItems);
         }
-
+        //<summary>
+        //Tests the behaviour of the Pack class when volume constraints is exceeded
+        //</summary>
         [TestMethod]
         public void VolumeOverflowTest()
         {
@@ -25,7 +31,69 @@ namespace AssignmentTest
 
             Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
             Assert.AreEqual(pack.Add(new Bow()), true);
-            Assert.AreEqual(pack.Add(new Bow()), false);
+
+        }
+        //<summary>
+        //Tests the behaviour of the Pack class when weight constraints is exceeded
+        //</summary>
+        [TestMethod]
+        public void WeightOverflowTest()
+        {
+            const int PackMaxItems = 1000;
+            const float PackMaxVolume = 1000;
+            const float PackMaxWeight = 10;
+
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.IsFalse(pack.Add(new Sword()));
+
+        }
+        //<summary>
+        //Tests the behaviour of the Pack class when count constraints is exceeded
+        //</summary>
+        [TestMethod]
+        public void CountOverflowTest()
+        {
+            const int PackMaxItems = 2;
+            const float PackMaxVolume = 10;
+            const float PackMaxWeight = 10;
+
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.IsTrue(pack.Add(new Arrow()));
+            Assert.IsTrue(pack.Add(new Arrow()));
+            Assert.IsFalse(pack.Add(new Arrow()));
+        }
+        //<summary>
+        //Tests the behaviour of the Pack class when volume and  weight constraints are both zero
+        //</summary>
+        [TestMethod]
+        public void ZeroVolumeAndWeightTest()
+        {
+            const int PackMaxItems = 100;
+            const float PackMaxVolume = 0;
+            const float PackMaxWeight = 0;
+
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.IsFalse(pack.Add(new Food()));
+
+        }
+        //<summary>
+        //Tests the behaviour of the Pack class when adding an item with negative weight and volume
+        //Expects ArgumentOutOfRangeException to be thrown.
+        //</summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void InvalidVolumeAndWeightTest()
+        {
+            const float NegativeWeight = -1.0f;
+            const float NegativeVolume = -0.5f;
+
+            const int PackMaxItems = 100;
+            const float PackMaxVolume = 100;
+            const float PackMaxWeight = 100;
+
+            Pack pack = new Pack(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            pack.Add(new Arrow(NegativeWeight, NegativeVolume));
+
         }
     }
 }
